6553b4fe59ad54055799607e2454e37c3789f629
Michael Torrie <torriem@gmail.com>
2025-02-10 21:18:31 -0700

    New unified settings class that will work in QML as well as C++. No more parsing AOG settings xml files.  Settings are now manually updated in settings.csv file.

---
+++ newsettings.cpp
@@ -0,0 +1,203 @@
+#include "newsettings.h"
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QFile>
+#include <QRect>
+
+QVector<int> default_relay_pinConfig = { 1,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+QVector<int> default_zones = { 2,10,20,0,0,0,0,0,0 };
+
+NewSettings::NewSettings(QObject *parent)
+    : QQmlPropertyMap{parent}
+{
+    connect (this, &QQmlPropertyMap::valueChanged,
+            this, &NewSettings::onValueChanged);
+}
+
+void NewSettings::addKey(const QString settings_key,
+                         const QVariant &default_value,
+                         NewSettings::SpecialCase special_case)
+{
+    //put key into app-wide settings with its default value,
+    //if it's not there already.  If it's already in QSettings, let's
+    //make sure it's the correct type
+    QVariant settings_value = settings.value(settings_key, default_value);
+
+    special_case_map.insert(settings_key, special_case);
+
+    //INI files are typeless, so when settings are read in when QSettings
+    //settings is constructed, some of the types end up being wrong.  In
+    //a couple of cases we need to correct them back to QVariantLists.
+
+    //this one should be a qvariant list, but ini reads it in as a QStringList
+    if (special_case == VECTOR_OF_INTS && settings_value.typeName() == "QStringList" ) {
+        QVariantList l;
+        for(QString &i: settings_value.toStringList()) {
+            l.append(QVariant(i.toInt()));
+        }
+        settings_value = l;
+    }
+
+    //set the possibly changed value in both settings and the hash map
+    setValue(settings_key, settings_value);
+}
+
+void NewSettings::onValueChanged(const QString &qml_key,
+                                 const QVariant &value)
+{
+    QString settings_key = qml_key.split('_').join('/');
+    int special_case = special_case_map[settings_key];
+
+    QString type_name = value.typeName();
+
+    if (type_name == "QJSValue" && special_case == VECTOR_OF_INTS) {
+        QVector<int> v = toVector<int>(value);
+        settings.setValue(settings_key, toVariant(v));
+    } else {
+        settings.setValue(settings_key, value);
+    }
+}
+
+QVariant NewSettings::value(const QString &key)
+{
+    return settings.value(key);
+}
+
+QVector<int> NewSettings::valueIntVec(const QString &key)
+{
+    QVariant val;
+    val = settings.value(key);
+    return toVector<int>(val);
+}
+
+void NewSettings::setValue(const QString &key, const QVariant &value)
+{
+    settings.setValue(key, value);
+
+    //in qml we use underscores instead of slashes.
+    QString qml_key = key.split('/').join('_');
+    insert(qml_key, value);
+}
+
+void NewSettings::setValue(const QString &key, const QVector<int> &value_list)
+{
+    QVariant qv = toVariant(value_list);
+    settings.setValue(key, qv);
+
+    //in qml we use underscores instead of slashes.
+    QString qml_key = key.split('/').join('_');
+    insert(qml_key, qv);
+}
+
+QJsonObject NewSettings::toJson()
+{
+    //b = QVariant(QColor::fromRgbF(1,0.5,0.2));
+    QVariant b;
+
+    QStringList keys = settings.allKeys();
+    QString type;
+    QString json_value;
+    QJsonObject blah; // = QJsonObject::fromVariantMap(keysValuesPairs);
+
+    for (const auto &key : keys)
+    {
+        b = settings.value(key);
+
+        type = b.typeName();
+
+        if (type == "QStringList" ||
+            type == "QVariantList"  ||
+            type == "QJSValue")
+        {
+            QVector<int> list = toVector<int>(b);
+            json_value="@List:";
+            for(int i=0; i < list.length(); i++){
+                json_value += QString("%1").arg(list[i]);
+                if (i < list.length() -1)
+                    json_value +=",";
+            }
+            blah[key] = json_value;
+        } else if (type == "QPoint") {
+            QByteArray raw_value;
+            QDataStream ds(&raw_value,QIODevice::WriteOnly);
+
+            ds << b;
+
+            json_value = QLatin1String("@Variant(");
+            json_value += QString::fromLatin1(raw_value.constData(), raw_value.size());
+            json_value += ")";
+            blah[key] = json_value;
+        } else {
+            blah[key] = QJsonValue::fromVariant(b);
+        }
+    }
+
+    return blah;
+}
+
+bool NewSettings::loadJson(QString filename)
+{
+    QFile loadfile(filename);
+    if (!loadfile.open(QIODevice::ReadOnly))
+    {
+        qWarning() << "Could not load json settings file " << filename;
+        return false;
+    }
+
+    QByteArray loadedjson = loadfile.readAll();
+    QJsonDocument loaded(QJsonDocument::fromJson(loadedjson));
+    QJsonObject j = loaded.object();
+    QString new_value;
+    QVariant v;
+
+    //load all the settings from json and put them in our
+    //QSettings and also this property hash map.
+    for (const auto &key : j.keys())
+    {
+        new_value = j[key].toString();
+        if (new_value.startsWith("@Variant("))
+        {
+            QByteArray raw_data;
+            QDataStream ds(&raw_data,QIODevice::ReadOnly);
+
+            raw_data = new_value.toLatin1().mid(9);
+            ds >> v;
+            setValue(key, v);
+        } else if(new_value.startsWith("@List:")) {
+            new_value = new_value.mid(6);
+            QStringList parts = new_value.split(",");
+            QVector<int> list;
+            for(QString part: parts) {
+                list.append(part.toInt());
+            }
+
+            setValue(key,list);
+        } else {
+            //if (key == "display/isMetric")
+            //    qDebug() << "isMetric is a problem child.";
+            v = j[key].toVariant();
+            setValue(key, v);
+        }
+    }
+
+    return true;
+}
+
+bool NewSettings::saveJson(QString filename)
+{
+    QFile savefile(filename);
+    if (!savefile.open(QIODevice::WriteOnly))
+    {
+        qWarning() << "Could not save json settings file " << filename;
+        return false;
+    }
+
+    savefile.write(QJsonDocument(toJson()).toJson());
+    savefile.close();
+
+    return true;
+
+}
+
+
---
+++ newsettings.h
@@ -0,0 +1,98 @@
+#ifndef NEWSETTINGS_H
+#define NEWSETTINGS_H
+
+#include <QObject>
+#include <QQmlPropertyMap>
+#include <QVariant>
+#include <QSettings>
+#include <QColor>
+#include <QPoint>
+#include <QSize>
+
+extern QVector<int> default_relay_pinConfig;
+extern QVector<int> default_zones;
+
+/* This class helps bridge between QSettings and QML/Javascript.
+ * An instance of QSettings is kept inside this class and a
+ * QSettings-like api is exported. This class is generally only
+ * instantiated once and hopefully the instance can be registered
+ * with qml using qmlSetSingletonInstance().
+ *
+ * The class must be initialized with all of the keys we're going
+ * to use, along with their types and default values.  Types are
+ * required because we have to convert some values when they are
+ * set in Javascript.  addKey() will do this for a given key.
+ *
+ * Anytime setValue() is called QSettings gets updated and also the
+ * hash map is updated, triggering binding updates in QML.  Any time
+ * QML changes a property in this map, we get a signal and can then
+ * update QSettings.
+ */
+
+class NewSettings : public QQmlPropertyMap
+{
+    Q_OBJECT
+
+    enum SpecialCase {
+        NORMAL = 0,
+        VECTOR_OF_INTS = 1,
+    };
+
+
+private:
+    QSettings settings;
+    QMap<QString, QMetaType::Type> settings_type_map;
+    QMap<QString, SpecialCase> special_case_map;
+
+public:
+    explicit NewSettings(QObject *parent = nullptr);
+
+
+    //all keys must be predeclared by calling this method
+    void setupKeys();
+
+    void addKey(const QString qsettings_key,
+                const QVariant &default_value,
+                NewSettings::SpecialCase special_case = NORMAL);
+
+    QVariant value(const QString &key);
+    QVector<int> valueIntVec(const QString &key);
+    void setValue(const QString &key, const QVariant &value);
+    void setValue(const QString &key, const QVector<int> &value_list);
+
+    QJsonObject toJson();
+    bool saveJson(QString filename);
+    bool loadJson(QString filename);
+
+public slots:
+    void onValueChanged (const QString &qml_key, const QVariant &value);
+
+};
+
+template <class T> static QVariant toVariant(const QVector<T> &list)
+{
+    QVariantList variantList;
+    variantList.reserve(list.size());
+    for (const auto& v : list)
+    {
+        variantList.append(v);
+    }
+    return variantList;
+}
+
+template <class T> static QVector<T> toVector(const QVariant &qv)
+{
+    QVector <T> dataList;
+    foreach(QVariant v, qv.value<QVariantList>()) {
+        dataList << v.value<T>();
+    }
+    return dataList;
+}
+
+/*
+QColor parseColor(QString setcolor);
+QVector3D parseColorVector(QString setcolor);
+int colorSettingStringToInt(QString colorSettingString);
+*/
+
+#endif // NEWSETTINGS_H
