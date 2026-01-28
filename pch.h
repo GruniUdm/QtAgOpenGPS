dc2e71a3f11d8d73a1a9108a6d3d60f083735243
Michael Torrie <torriem@gmail.com>
2026-01-27 22:33:49 -0700

    Turned on precompiled headers for Qt headers and stdlib headers. Hopefully that will speed up build times a little bit.

---
+++ pch.h
@@ -0,0 +1,98 @@
+// Copyright (C) 2024 Michael Torrie and the QtAgOpenGPS Dev Team
+// SPDX-License-Identifier: GNU General Public License v3.0 or later
+//
+// Precompiled header - stable Qt and standard library headers
+// Regenerate by touching this file: touch pch.h
+
+#ifndef PCH_H
+#define PCH_H
+
+// ============================================================================
+// Qt Core
+// ============================================================================
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QList>
+#include <QVector>
+#include <QMap>
+#include <QHash>
+#include <QSet>
+#include <QVariant>
+#include <QVariantList>
+#include <QDebug>
+#include <QLoggingCategory>
+#include <QPointer>
+#include <QScopedPointer>
+#include <QSharedPointer>
+#include <QMutex>
+#include <QTimer>
+#include <QFile>
+#include <QDir>
+#include <QFileInfo>
+#include <QSettings>
+#include <QCoreApplication>
+
+// Qt 6 Bindable Properties
+#include <QProperty>
+#include <QBindable>
+
+// ============================================================================
+// Qt GUI
+// ============================================================================
+#include <QColor>
+#include <QMatrix4x4>
+#include <QVector2D>
+#include <QVector3D>
+#include <QVector4D>
+#include <QPointF>
+#include <QRectF>
+#include <QSizeF>
+#include <QImage>
+#include <QGuiApplication>
+
+// ============================================================================
+// Qt Quick / Scene Graph
+// ============================================================================
+#include <QQuickItem>
+#include <QQuickWindow>
+#include <QSGNode>
+#include <QSGGeometry>
+#include <QSGGeometryNode>
+#include <QSGTransformNode>
+#include <QSGMaterial>
+#include <QSGMaterialShader>
+#include <QSGTexture>
+#include <QSGFlatColorMaterial>
+#include <QSGTextureMaterial>
+#include <QSGOpaqueTextureMaterial>
+
+// ============================================================================
+// Qt QML
+// ============================================================================
+#include <QQmlEngine>
+#include <QQmlContext>
+#include <QQmlComponent>
+#include <QQmlApplicationEngine>
+#include <QQmlListProperty>
+#include <QtQml/qqmlregistration.h>
+
+// ============================================================================
+// Qt Network
+// ============================================================================
+#include <QUdpSocket>
+#include <QHostAddress>
+#include <QNetworkDatagram>
+
+// ============================================================================
+// C++ Standard Library
+// ============================================================================
+#include <memory>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <cmath>
+#include <limits>
+
+#endif // PCH_H
